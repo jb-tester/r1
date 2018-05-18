b3c95c78a1408a4767aa51c0335097970b9727a1
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-18 14:45:55 +0300

    add Foo1

---
+++ src/a/b/c/Foo1.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 5/18/2018.
+ * Project: r1_clone1
+ * *******************************
+ */
+public class Foo1 {
+}
