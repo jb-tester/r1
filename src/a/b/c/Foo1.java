9bdc79cd155dbecdbaab009dfc2c0630e952c9c0
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-18 16:33:28 +0300

    multipush

--- src/a/b/c/Foo1.java
+++ src/a/b/c/Foo1.java
@@ -10,17 +10,18 @@ public class Foo1 {
     /**
      * str1...
      */
-
    private String str1;
     /**
-     *    str1
-     *
+     *    iii
      */
-
    private int iii;
 
     public Foo1(String str1, int iii) {
         this.str1 = str1;
         this.iii = iii;
+    }
+
+    public int getIii() {
+        return iii;
     }
 }
---
+++ src/a/b/c/Foo3.java
@@ -0,0 +1,21 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 5/18/2018.
+ * Project: hg_and_git
+ * *******************************
+ */
+public class Foo3 {
+
+    public void method1(String arg) {
+        /*
+      bar field
+     */ /**
+         * bar field
+         */String bar = arg.toLowerCase();
+        System.out.println(bar);
+    }
+
+    ;
+}
