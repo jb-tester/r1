48306fc32187ee8834a9992b38de3612be246518
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-18 15:28:29 +0300

    b1: accessors for fields

--- src/a/b/c/Foo1.java
+++ src/a/b/c/Foo1.java
@@ -12,6 +12,23 @@ public class Foo1 {
      */
 
     String str1;
+
+    public String getStr1() {
+        return str1;
+    }
+
+    public void setStr1(String str1) {
+        this.str1 = str1;
+    }
+
+    public int getIii() {
+        return iii;
+    }
+
+    public void setIii(int iii) {
+        this.iii = iii;
+    }
+
     /**
      * str1
      */
