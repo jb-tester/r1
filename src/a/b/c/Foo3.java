aa47701db0dc1f7a522884bcbaac72800d102246
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-08-06 12:49:41 +0300

    Foo4.java, Foo3.java
    Signed-off-by: irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>

--- src/a/b/c/Foo3.java
+++ src/a/b/c/Foo3.java
@@ -2,7 +2,7 @@ package a.b.c;
 
 /**
  * *******************************
- * Created by Irina Petrovskaya on 5/18/2018.
+ * Created by Irina.Petrovskaya on 5/18/2018.
  * Project: hg_and_git
  * *******************************
  */
@@ -10,9 +10,11 @@ public class Foo3 {
     public Foo3() {
     }
     /**
-     * @param arg: my dummy parameter,  some string
+     *
+     *
+     * @param arg - some string
      */
-    public void method1111(String arg, int aaa) {
+    public void method1(String arg) {
         String bar = arg.toLowerCase();
         System.out.println(bar);
     }
--- src/a/b/c/Foo4.java
+++ src/a/b/c/Foo4.java
@@ -2,14 +2,14 @@ package a.b.c;
 
 /**
  * *******************************
- * Created by Irina Petrovskaya on 8/3/2018.
+ * Created by Irina.Petrovskaya on 8/3/2018.
  * Project: r1_clone1
  * *******************************
  */
 public class Foo4 {
 
 
-    int i1; //
+    int i1; // iii
 
     public int getI1() {
         return i1;
