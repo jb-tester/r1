79bf17a2efc625d5e460f0b2a4ea13f8341f074f
irina <irina@me.com>
2019-04-19 13:53:59 +0300

    Foo1.java rename...

--- src/a/b/c/Foo1.java
+++ src/a/b/c/Foo1.java
@@ -4,6 +4,8 @@ package a.b.c;
  * *******************************
  * Created by Irina.Petrovskaya on 5/18/2018.
  * Project: r1_clone1
+ *
+ *
  * *******************************
  */
 public class Foo1 {
