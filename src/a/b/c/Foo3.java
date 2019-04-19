8d74b9f9e011d3a8fd8b4b15995335485b3ab106
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2019-04-19 14:18:09 +0300

    rename project in comments
    Signed-off-by: irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>

--- src/a/b/c/Foo3.java
+++ src/a/b/c/Foo3.java
@@ -3,7 +3,7 @@ package a.b.c;
 /**
  * *******************************
  * Created by Irina.Petrovskaya on 5/18/2018.
- * Project: r1_clone1
+ * Project: r1
  * *******************************
  */
 public class Foo3 {
